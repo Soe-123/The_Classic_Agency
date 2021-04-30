81890af3a14b6045a998a65b24220b635edf6cd8
Soe-123 <82047482+Soe-123@users.noreply.github.com>
2021-04-30 15:56:30 +0100

    Major update to carousel

---
+++ owl.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
+
+  function vh(v) {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (v * h) / 100;
+  }
+
+  $('.owl-carousel').owlCarousel({
+        item:3,
+        singleItem: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause:true,
+        loop: true,
+        nav: false,
+        responsiveClass: true,
+        margin: 10,
+        responsive:{
+            0:{
+                items: 1,
+                dots: false
+            },
+            450:{
+                items: 2,
+                dots: false
+            },
+            650:{
+                items: 3,
+                dots: false
+            },
+            1024:{
+                items: 4
+            }
+
+        }
+    });
+
